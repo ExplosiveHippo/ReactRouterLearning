8d2b10ee8cffc52c13e27e020e736bf3ba189097
Lance Laughlin <lance.laughlin@cvs.com>
2017-08-04 14:32:35 -0400

    more work

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,84 @@
 import React, {Component} from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
-	render() {
+	
+	renderField(field) {
+
+		const { meta: { touched, error } } = field;
+		const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
 		return (
-			<div>
-				<h3>PostsNew!</h3>
+			<div className={className}>
+				<label>{field.label}</label>
+				<input
+					className="form-control"
+					type="text"
+					{...field.input}
+				/>
+				<div className="text-help">
+					{touched ? error : ''}
+				</div>
 			</div>
+		);
+	}
+
+
+	onSubmit(values) {
+		// this === component
+		console.log(values);
+	}
+
+	render() {
+
+		const { handleSubmit } = this.props;
+
+		return (
+			<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+				<Field
+					name="title"
+					label="Title"
+					component={this.renderField}
+				/>
+				<Field
+					name="categories"
+					label="Categories"
+					component={this.renderField}
+				/>
+				<Field
+					name="content"
+					label="Post Content"
+					component={this.renderField}
+				/>
+				<button type="submit" className="btn btn-primary">Submit</button>
+				<Link className="btn btn-danger" to="/posts/">Cancel</Link>
+			</form>
 		)
 	}
 }
 
-export default PostsNew;
+function validate(values) {
+	const errors = {};
+
+	if (!values.title || values.title.length < 3) {
+		errors.title = "Enter a title that is at least 3 characters";
+	}
+
+	if (!values.categories) {
+		errors.categories = "Enter a categories";
+	}
+
+	if (!values.content) {
+		errors.content = "Enter a content";
+	}
+
+	return errors;
+}
+
+export default reduxForm({
+	validate,
+	form: 'PostsNewForm'
+})(PostsNew);
